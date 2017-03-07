3fb885c8e670223b77c853be8efaab8925b92026
rajat-garg <rg6693@gmail.com>
2017-03-08 01:40:09 +0530

    Add testcases for division and multiplication

--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -36,4 +36,42 @@ public class CalculatorTest {
     public void testSubFail(){
         assertThat("Test Fails",Calculator.sub(2,1),is(not(6)));
     }
+
+    @Test
+    public void testDivIntPass(){
+        assertEquals("Error in division()",3,Calculator.divInt(9,3));
+        assertEquals("Error in division()",0,Calculator.divInt(1,6));
+    }
+
+    @Test
+    public void testDivIntFail(){
+        assertThat("Test Fails",Calculator.divInt(4,2),is(not(3)));
+    }
+
+    @Test
+    public void testDivRealPass(){
+        assertEquals("Error in Division()",0.333333,Calculator.divReal(1,3),1e-6);
+        assertEquals("Error in division()",0.111111,Calculator.divReal(1,9),1e-6);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivIntByZero(){
+        Calculator.divInt(9,0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivRealByZero(){
+        Calculator.divReal(4,0);
+    }
+
+    @Test
+    public void testMulPass(){
+        assertEquals("Error in Multiply()",6,Calculator.mul(2,3));
+        assertEquals("Error in Multiply()",78,Calculator.mul(2,39));
+    }
+
+    @Test
+    public void testMulFail(){
+        assertThat("Error in Multiply()",Calculator.mul(6,11),is(not(696)));
+    }
 }
